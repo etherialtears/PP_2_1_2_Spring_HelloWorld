20cc218db4ce681d55f5f48eb8159b41b1683053
Роман Вырлан <vyrlan81@mail.ru>
2026-04-27 00:26:28 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beancat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beancat.getName());
+
+        Cat beancat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beancat1.getName());
+
+        System.out.println(bean == bean1);
+        System.out.println(beancat == beancat1);
+
     }
 }
